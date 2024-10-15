2a9164e3e347747056512e53b283867f59986cce
Julian K. Arni <jkarni@gmail.com>
2024-10-15 17:57:28 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
               # You only need to change this if you changed the forked repo name.
               githubRepo = "nixcon-2024-player-template";
               # You only need to change this if you changed the forked repo name.
-              webserver = "${self.packages.x86_64-linux.default}/bin/nixcon-talk";
+              webserver = self.packages.x86_64-linux.default;
               # If you want to log in to your deployed server, put your SSH key
               # here:
               sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVpNqdbM7uE1xkKoXztoaAtKtDHoqHS3DrzxYKsDgxa jkarni@garnix.io";
